6d233ca1752cb9e55f682ce4aa4126139a2824ac
charan <charan.manigandan@gmail.com>
2023-09-02 14:36:11 +0530

    ToDo

---
+++ task/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+# url routing
+urlpatterns = [
+    path('', views.index, name='list'),
+    path('update_task/<str:pk>/', views.update_task, name='update_task'),
+    path('delete/<str:pk>/', views.deleteTask, name='delete')
+]
